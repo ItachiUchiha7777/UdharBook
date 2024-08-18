c258129c6ea211e3c464eb363a4a0e51ccd6b1c6
Rohit Gusain <98213628+ItachiUchiha7777@users.noreply.github.com>
2024-08-18 23:40:44 +0530

    name changes

--- components/Home.js
+++ components/Home.js
@@ -11,9 +11,9 @@ const DATA_KEY = 'transactions';
 const demoData = [
   { id: '1', name: "Rohit", direction: "In", amount: 15, status: 0 },
   { id: '2', name: "Ishan Chaudhary", direction: "Out", amount: 20, status: 1 },
-  { id: '3', name: "Divya R", direction: "In", amount: 25, status: 0 },
-  { id: '4', name: "Ekterina R", direction: "Out", amount: 30, status: 1 },
-  { id: '5', name: "Nihrarika R", direction: "In", amount: 35, status: 0 },
+  { id: '3', name: "Divya ", direction: "In", amount: 25, status: 0 },
+  { id: '4', name: "Ekterina ", direction: "Out", amount: 30, status: 1 },
+  { id: '5', name: "Nihrarika ", direction: "In", amount: 35, status: 0 },
   { id: '6', name: "Aahana", direction: "Out", amount: 40, status: 1 },
   { id: '7', name: "Karan", direction: "In", amount: 45, status: 0 },
   { id: '8', name: "Priya", direction: "Out", amount: 50, status: 1 },
